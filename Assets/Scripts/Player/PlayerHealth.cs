cc95acfa9fad0019f557c409ee14fb6a1fe74847
ibra <muhammadibrahimhisham@gmail.com>
2022-07-17 12:09:40 +0500

    start work on healthbar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,16 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Casino.Player
 {
     public class PlayerHealth : MonoBehaviour
     {
         private float _health;
+        private Image[] healthImages;
 
+        [SerializeField] private GameObject healthBar;
+        [SerializeField] private Image[] healthQuarters;
         [SerializeField] private float maxHealth = 30f;
-
+        
         private void Start()
         {
             _health = maxHealth;
+            healthImages = healthBar.GetComponentsInChildren<Image>();
+        }
+
+        private void Update()
+        {
+               
         }
 
         public void TakeDamage(float damage)
